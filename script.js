b9a950630b3cfc0a58f2d03ead7418909c41cbef
macaulam <119492189+macaulam@users.noreply.github.com>
2023-01-27 18:53:10 +0000

    First javascript update

---
+++ script.js
@@ -0,0 +1,10 @@
+// Aquire the time and date
+// Show the current day and time on page
+// Create array for the work hours (9am-5pm)
+// Loop through work hours and create timeblocks for each hour
+  // Create new row for each timeblock
+  // Append hour, description, and save button to row
+  // Append row to container
+  // Check if current time is past, present, or future
+// Save button click event
+// Retrieve saved data from local storage and display in appropriate timeblock
